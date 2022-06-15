708ae98cadcbef7c3068eead93afe8f31f60cac3
rhashymov <levelend131@gmail.com>
2022-06-15 13:39:36 +0300

    alert

--- src/cmp/Cosmetics/Cosmetics.jsx
+++ src/cmp/Cosmetics/Cosmetics.jsx
@@ -1,10 +1,10 @@
 import { useDispatch } from "react-redux"
 import { getCosmetics } from "../../redux/reducer"
-import { Row, Input, Space } from 'antd'
+import { Row, Input, Space, Alert } from 'antd'
 import { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
 import Items from "./Items/Items";
-import {getSearch} from './../../redux/searchReducer'
+import styles from './Cosmetics.module.css'
 const { Search } = Input;
 
 
@@ -12,9 +12,10 @@ const PaginatedItems = () => {
     const itemsPerPage = 30;
     const dispatch = useDispatch()
     const [items, setItems] = useState([])
+    const [deafaultItems, setDeafaultItems] = useState([])
     const [currentItems, setCurrentItems] = useState(null);
     const [pageCount, setPageCount] = useState(0);
-
+    const [error, setError] = useState(false)
     const [itemOffset, setItemOffset] = useState(0);
 
 
@@ -22,69 +23,77 @@ const PaginatedItems = () => {
         async function asyncFn() {
             const res = await dispatch(getCosmetics())
             setItems(res)
+            setDeafaultItems(res)
         }
         asyncFn();
     }, [])
 
     useEffect(() => {
         const endOffset = itemOffset + itemsPerPage;
-        console.log(`Loading items from ${itemOffset} to ${endOffset}`);
         setCurrentItems(items.slice(itemOffset, endOffset));
         setPageCount(Math.ceil(items.length / itemsPerPage));
     }, [items, itemOffset, itemsPerPage]);
 
     const handlePageClick = (event) => {
         const newOffset = (event.selected * itemsPerPage) % items.length;
-        console.log(
-            `User requested page number ${event.selected}, which is offset ${newOffset}`
-        );
+        
         setItemOffset(newOffset);
     };
     const getC = (value) => {
-        return dispatch(getSearch(value)).then(response => {
-            setItems(response)
+        if (value) {
+            const res = items.filter(item => item.name.toLowerCase().includes(value.toLowerCase()))
+            if (res.length) {
+                setItems(res)
+                setError(false)
+            } else {
+                setError(true)
             }
-        )
+        }
+        else {
+            setItems(deafaultItems)
+            setError(false)
+        }
+
     }
     return (
         <>
-        <div className="site-card-wrapper">
-        <Space direction="vertical">
-    <Search
-      placeholder="input search text"
-      allowClear
-      enterButton="Search"
-      size="large"
-      onSearch={getC}
-    />
- 
-  </Space>
-            <Row gutter={[48, 16]}>
-                <Items currentItems={currentItems} />
-            </Row>
-            <ReactPaginate
-                previousLabel="Previous"
-                nextLabel="Next"
-                pageClassName="page-item"
-                pageLinkClassName="page-link"
-                previousClassName="page-item"
-                previousLinkClassName="page-link"
-                nextClassName="page-item"
-                nextLinkClassName="page-link"
-                breakLabel="..."
-                breakClassName="page-item"
-                breakLinkClassName="page-link"
-                pageCount={pageCount}
-                marginPagesDisplayed={2}
-                pageRangeDisplayed={5}
-                onPageChange={handlePageClick}
-                containerClassName="pagination"
-                activeClassName="active"
-            />
-        </div>
-      
+            <div className="site-card-wrapper">
+                <Space direction="vertical">
+                    <Search
+                        placeholder="input search text"
+                        allowClear
+                        enterButton="Search"
+                        size="large"
+                        onSearch={getC}
+                    />
+                    <Alert message="Error Text" type="error" className={error ? styles.showAlert : styles.hideAlert} />
+                </Space>
+                <Row gutter={[48, 16]}>
+                    <Items currentItems={currentItems} />
+                </Row>
+                <ReactPaginate
+                    previousLabel="Previous"
+                    nextLabel="Next"
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item"
+                    nextLinkClassName="page-link"
+                    breakLabel="..."
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    pageCount={pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageClick}
+                    containerClassName="pagination"
+                    activeClassName="active"
+                />
+            </div>
+
         </>
-        
+
     );
 }
 
