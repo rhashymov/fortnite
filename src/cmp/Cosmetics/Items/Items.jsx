06ede4d0f8e2e9d4141137033198f93bc00c681d
Shyshkov Denys <Denys.Shyshkov@kingfisher.com>
2022-06-22 16:11:44 +0300

    review comments

--- src/cmp/Cosmetics/Items/Items.jsx
+++ src/cmp/Cosmetics/Items/Items.jsx
@@ -1,47 +1,66 @@
-import { Col, Modal } from 'antd'
-import { useState } from 'react';
-import styles from './Items.module.css'
+import { Col, Modal } from "antd";
+import { useState } from "react";
+import styles from "./Items.module.css";
 
 const Items = ({ currentItems }) => {
-    const [isModalVisible, setIsModalVisible] = useState();
-    const [popupData, setPopupData] = useState({
-        name: null,
-        title: null,
-        images: {
-            icon: null
-        }})
+  const [isModalVisible, setIsModalVisible] = useState();
+  const [popupData, setPopupData] = useState({
+    name: null,
+    title: null,
+    images: {
+      icon: null,
+    },
+  });
 
-    const showModal = (item) => {
-        setPopupData(item)
-        setIsModalVisible(true);
-    };
+  const showModal = (item) => {
+    setPopupData(item);
+    setIsModalVisible(true);
+  };
 
-    const handleOk = () => {
-        setIsModalVisible(false);
-    };
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
 
-    const handleCancel = () => {
-        setIsModalVisible(false);
-    };
-    return (<>
-        {currentItems && currentItems.map((item) => (
-            <>
-                <Col span={4}>
-                    <div className={styles.card} key={item.id} onClick={() => showModal(item)}>
-                        <div><img alt='cosmetics' className={styles.banner} src={item.images.smallIcon} /></div>
-                        <div> {item.title}</div>
-                        <div className={styles.itemName}>{item.name}</div>
-                        <div>{item.description}</div>
-                    </div>
-                </Col>
-            </>
-       ))}
-        <Modal title={popupData.name} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-       <div>{popupData.title}</div>
-       <img alt='cosmetics' src={popupData.images.smallIcon} />
-        </Modal>
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  return (
+    <>
+      {currentItems &&
+        currentItems.map((item) => (
+          <>
+            <Col span={4}>
+              <div
+                className={styles.card}
+                key={item.id}
+                onClick={() => showModal(item)}
+              >
+                <div>
+                  <img
+                    alt="cosmetics"
+                    className={styles.banner}
+                    src={item.images.smallIcon}
+                  />
+                </div>
+                <div> {item.title}</div>
+                <div className={styles.itemName}>{item.name}</div>
+                <div>{item.description}</div>
+              </div>
+            </Col>
+          </>
+        ))}
+      <Modal
+        title={popupData.name}
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        {/* тут должно быть по больше инфы  */}
+        <div>{popupData.title}</div>
+        <img alt="cosmetics" src={popupData.images.smallIcon} />
+      </Modal>
     </>
-    )
-}
+  );
+};
 
-export default Items
+export default Items;
